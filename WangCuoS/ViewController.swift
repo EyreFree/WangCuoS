c68a9a2c37a44a49123ad1cc083e9959dd41ae26
EyreFree <EyreFree@163.com>
2015-07-31 13:38:44 +0800

    Initial Commit

---
+++ WangCuoS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WangCuoS
+//
+//  Created by Venpoo on 15/7/31.
+//  Copyright © 2015年 EyreFree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
